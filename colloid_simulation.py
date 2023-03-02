abebfee3d7abc18f6af005924ce05ddb280ee7c6
Jasper Katzban <jasperkatzban@gmail.com>
2023-03-02 00:56:30 -0500

    generate non-overlapping particles

--- colloid_simulation.py
+++ colloid_simulation.py
@@ -11,16 +11,18 @@ class Simulation:
     def __init__(self, config) -> None:
         """Set up simulation"""
         self.size = config["size"]
-        self.num_particles = config["num_particles"]
+        self.particle_concentration = config["particle_concentration"]
         self.particle_radius = config["particle_radius"]
-        self.particle_distribution = config["particle_distribution"]
+        self.particle_mass = config["particle_mass"]
         self.hydrodynamic_drag = config["hydrodynamic_drag"]
         self.brownian_amplitude = config["brownian_amplitude"]
         self.timestep = config["timestep"]
         self.collision_check_mode = config["collision_check_mode"]
         self.show_grid = config["show_grid"]
+        self.show_collisions = config["show_collisions"]
 
         self.particles = None
+        self.num_particles = 0
         self.bins = np.arange(
             0, self.size, self.particle_radius * 2)
 
@@ -33,9 +35,9 @@ class Simulation:
 
     def add_particle(self, pos: tuple, mass: float, radius: float) -> None:
         """
-        Adds a particle to the simulation's array of particles.
+        Adds a particle to the simulation's array of particles
 
-        Particle has no initial velocity as this is computed as brownian motion
+        Particle has no initial velocity as this is computed as brownian motion each time step
         """
         vel_x, vel_y = 0, 0
 
@@ -47,6 +49,42 @@ class Simulation:
         else:
             self.particles = np.ndarray(
                 shape=(1, new_particle_data.shape[0]), buffer=new_particle_data)
+
+    def create_2_rows_of_coords(self, x, y, width, spacing) -> np.ndarray:
+        """
+        Generate 2 rows of coordinates offset hexagonally
+
+        Returns a 2D numpy array of coordinates of two rows of particles
+        hexagonally offset bounded by a set width
+        """
+        coords = []
+        current_x_coord = x
+        while current_x_coord <= width + x:
+            # first row
+            coords.append((current_x_coord, y))
+            # second row
+            # TODO: ensure second offset particles don't exceed width.
+            # Could refactor to generate spaced coordinate points in a single function
+            coords.append((current_x_coord + spacing / 2,
+                           y + np.sqrt(3) * spacing / 2))
+            current_x_coord += spacing
+        return np.array(coords)
+
+    def build_hex_coords(self, x, y, width, height, spacing) -> np.ndarray:
+        """
+        Generate coordinates for a hex grid of particles
+
+        Returns a 2D numpy array of coordinates of rows of particles
+        hexagonally offset 
+        """
+        coords = []
+        current_y_coord = y
+        while current_y_coord <= height + y:
+            rows = self.create_2_rows_of_coords(
+                x, current_y_coord, width, spacing)
+            coords.append(rows)
+            current_y_coord += np.sqrt(3) * spacing
+        return np.vstack(coords)
 
     def generate_brownian_velocity(self, brownian_amplitude) -> np.ndarray:
         """Generates brownian motion velocity vector components"""
@@ -68,7 +106,7 @@ class Simulation:
     def update_particles(self) -> None:
         """Updates particle positions"""
         # compute new particle positions
-        vel = self.particles[:, 2:4]
+        vel = self.particles[:, 2: 4]
         d_pos = vel * self.timestep
         d_pos = np.pad(d_pos, ((0, 0), (0, 5)), mode='constant',
                        constant_values=(0, 0))
@@ -77,7 +115,7 @@ class Simulation:
         self.particles = self.particles + d_pos
 
     def apply_brownian_velocity(self) -> None:
-        """Applies brownain velocity component to particles"""
+        """Applies brownian velocity component to particles"""
 
         self.particles[:, 2:4] += self.generate_brownian_velocity(
             self.brownian_amplitude)
@@ -88,26 +126,17 @@ class Simulation:
 
     def run(self) -> None:
         """Run and plot simulation"""
-        # add particles
-        if self.particle_distribution == "random":
-            for _ in range(self.num_particles):
-                self.add_particle(
-                    (np.random.rand(2,) - .5) * self.size + self.size / 2,
-                    1.,
-                    self.particle_radius
-                )
-        # TODO: Distribute particles evenly on grid based on concentration without overlapping
-        # elif self.particle_distribution == "grid":
-        #     it probably makes more sense to define particle positions with concentration and not number
-        #     spacing = self.size / self.num_particles
-        #     for x in range(self.num_particles):
-        #         for y in range(self.num_particles // 2):
-        #             self.add_particle((x * spacing, y * spacing),
-        #                               1., self.particle_radius)
+        # add particles in a hexagon grid pattern
+        hex_coords = self.build_hex_coords(
+            0, 0, self.size, self.size, self.particle_concentration)
+        for coord in hex_coords:
+            self.add_particle(coord, self.particle_mass, self.particle_radius)
+            self.num_particles += 1
 
-        # continually loop
+        # continually loop to run simulation
         while True:
-            ### POSITION CALCULATION ###
+            ### POSITION CALCULATIONS ###
+
             self.apply_hydrodynamic_drag()
             self.apply_brownian_velocity()
 
@@ -117,17 +146,73 @@ class Simulation:
             # reset flag specifying if particles might be colliding
             self.particles[:, 6] = 0
 
+            if self.collision_check_mode == "spatial_distance":
+                # calculate pairwise distances with no repeats
+                particle_positions = self.particles[:, 0:2]
+                particle_distances = pdist(
+                    particle_positions)
+
+                # convert to square form matrix for ease of indexing pairs
+                particle_distances = squareform(particle_distances)
+
+                # exclude particles distances from themselves (zero) by setting
+                # their own distance to greater than the collision threshold
+                particle_distances += np.identity(self.num_particles) * \
+                    (2 * self.particle_radius + 1)
+
+                # check which particles are overlapping
+                particle_bitmask = particle_distances <= (
+                    2 * self.particle_radius)
+
+                # TODO: set the diagonal half of the matrix false here
+
+                # generate pairs based on truthy pairs of values
+                particles_indices_pairs_to_check = np.transpose(
+                    np.nonzero(particle_bitmask))
+
+                # TODO: instead of this, only use one diagonal half of matrix to avoid duplicate checks
+                particles_indices_pairs_to_check = [
+                    tuple(sorted(pair)) for pair in particles_indices_pairs_to_check]
+                particles_indices_pairs_to_check = list(set(
+                    particles_indices_pairs_to_check))
+
+                # generate list of all potentially colliding particles
+                all_colliding_particle_indices = [
+                    i for sublist in particles_indices_pairs_to_check for i in sublist]
+                all_colliding_particle_indices = list(
+                    set(all_colliding_particle_indices))
+
+                # set flag if particles might be colliding for visualization
+                self.particles[all_colliding_particle_indices, 6] = 1
+
+                # compute collision for sets of particles that are close enough to have them
+                for index_pair in particles_indices_pairs_to_check:
+                    particle_1 = self.particles[index_pair[0], :]
+                    particle_2 = self.particles[index_pair[1], :]
+
+                    # get necessary attributes for collision computation
+                    pos_1 = particle_1[0:2]
+                    pos_2 = particle_2[0:2]
+                    vel_1 = particle_1[2:4]
+                    vel_2 = particle_2[2:4]
+                    mass_1 = particle_1[4]
+                    mass_2 = particle_2[4]
+
+                    # calculate the new velocities after the collision
+                    vel_1_new = vel_1 - 2 * mass_2 / (mass_1 + mass_2) * np.dot(
+                        vel_1 - vel_2, pos_1 - pos_2) / np.linalg.norm(pos_1 - pos_2) ** 2 * (pos_1 - pos_2)
+                    vel_2_new = vel_2 - 2 * mass_1 / (mass_1 + mass_2) * np.dot(
+                        vel_2 - vel_1, pos_2 - pos_1) / np.linalg.norm(pos_2 - pos_1) ** 2 * (pos_2 - pos_1)
+
+                    # update particle velocities
+                    self.particles[index_pair[0], 2:4] = vel_1_new
+                    self.particles[index_pair[1], 2:4] = vel_2_new
             # TODO: this currently only checks if particle centers are in the same grid
             # square and misses many intersections when they are not.
-            if self.collision_check_mode == "bounding_boxes":
+            elif self.collision_check_mode == "bounding_boxes":
                 # quantize (digitize) particles into bins
                 digitized_particle_coords = np.digitize(
                     self.particles[:, 0:2] - self.particle_radius, self.bins, right=True)
-
-                # NOTE: unused but could be used for density calculations
-                # hist = np.histogram2d(
-                #     digitized_particle_coords[:, 0], digitized_particle_coords[:, 1])[0] // 1 - 1
-                # hist_collisions = hist != 0
 
                 # compute actual positions of bins and plot
                 binned_particle_pos = digitized_particle_coords * 2 * self.particle_radius
@@ -181,71 +266,6 @@ class Simulation:
                 # set flag if particles might be colliding
                 self.particles[all_colliding_particle_indices, 6] = 1
 
-            elif self.collision_check_mode == "spatial_distance":
-                # calculate pairwise distances with no repeats
-                particle_positions = self.particles[:, 0:2]
-                particle_distances = pdist(
-                    particle_positions)
-
-                # convert to square form matrix for ease of indexing pairs
-                particle_distances = squareform(particle_distances)
-
-                # exclude particles distances from themselves (zero) by setting their own distance to greater than the collision threshold
-                particle_distances += np.identity(self.num_particles) * \
-                    (2 * self.particle_radius + 1)
-
-                # check which particles are overlapping
-                particle_bitmask = particle_distances <= (
-                    2 * self.particle_radius)
-
-                # TODO: set the diagonal half of the matrix false here
-
-                # generate pairs based on truthy pairs of values
-                particles_indices_pairs_to_check = np.transpose(
-                    np.nonzero(particle_bitmask))
-
-                # TODO: instead of this, only use one diagonal half of matrix to avoid duplicate checks
-                particles_indices_pairs_to_check = [
-                    tuple(sorted(pair)) for pair in particles_indices_pairs_to_check]
-                particles_indices_pairs_to_check = list(set(
-                    particles_indices_pairs_to_check))
-
-                # generate list of all potentially colliding particles
-                all_colliding_particle_indices = [
-                    i for sublist in particles_indices_pairs_to_check for i in sublist]
-                all_colliding_particle_indices = list(
-                    set(all_colliding_particle_indices))
-
-                # set flag if particles might be colliding for visualization
-                self.particles[all_colliding_particle_indices, 6] = 1
-
-                # compute collision for sets of particles that are close enough to have them
-                for index_pair in particles_indices_pairs_to_check:
-                    particle_1 = self.particles[index_pair[0], :]
-                    particle_2 = self.particles[index_pair[1], :]
-
-                    # get necessary attributes for collision computation
-                    pos_1 = particle_1[0:2]
-                    pos_2 = particle_2[0:2]
-                    vel_1 = particle_1[2:4]
-                    vel_2 = particle_2[2:4]
-                    mass_1 = particle_1[4]
-                    mass_2 = particle_2[4]
-
-                    # calculate the new velocities after the collision
-                    vel_1_new = vel_1 - 2 * mass_2 / (mass_1 + mass_2) * np.dot(
-                        vel_1 - vel_2, pos_1 - pos_2) / np.linalg.norm(pos_1 - pos_2) ** 2 * (pos_1 - pos_2)
-                    vel_2_new = vel_2 - 2 * mass_1 / (mass_1 + mass_2) * np.dot(
-                        vel_2 - vel_1, pos_2 - pos_1) / np.linalg.norm(pos_2 - pos_1) ** 2 * (pos_2 - pos_1)
-
-                    # update particle velocities
-                    self.particles[index_pair[0], 2:4] = vel_1_new
-                    self.particles[index_pair[1], 2:4] = vel_2_new
-
-            # plot histogram of particle density per bin
-            # NOTE: unused and unscaled
-            # self.ax.matshow(hist * self.particle_radius)
-
             ### PLOTTING ###
 
             # clear graph and set limits
@@ -266,19 +286,23 @@ class Simulation:
             # calculate marker size on grid to match particle radius
             marker_radius = self.ax.transData.transform(
                 [self.particle_radius, 0])[0] - self.ax.transData.transform([0, 0])[0]
-            # TODO: this calculation is approximate and should be fixed
+            # this calculation approximates the
             marker_size = .42 * marker_radius**2
 
             # set particle color if it's being checked for collisions
-            particle_color = [
-                'orange' if state == 1 else 'blue' for state in self.particles[:, 6]]
+            if self.show_collisions:
+                particle_color = [
+                    'orange' if state == 1 else 'blue' for state in self.particles[:, 6]]
+            else:
+                particle_color = 'b'
 
             # plot particles
             self.ax.scatter(x, y, marker_size, color=particle_color, alpha=.25)
 
             # plot binned particles
-            # self.ax.scatter(
-            #     binned_particle_pos[:, 0], binned_particle_pos[:, 1], marker_size, color='g', marker='x')
+            if self.collision_check_mode == "bounding_boxes":
+                self.ax.scatter(
+                    binned_particle_pos[:, 0], binned_particle_pos[:, 1], marker_size, color='g', marker='x')
 
             # draw on canvas
             self.fig.canvas.draw()
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from colloid_simulation import Simulation
 # define simulation parameters
 config = {
     "size": 50,
-    "num_particles": 50,
+    "particle_concentration": 2.5,
     "particle_radius": 1.,
-    "particle_distribution": "random",
+    "particle_mass": 1.,
     "hydrodynamic_drag": .9,
     "brownian_amplitude": .4,
     "timestep": .1,
-    "collision_check_mode": 'spatial_distance',  # or spatial_distance
-    "show_grid": True
+    "collision_check_mode": 'spatial_distance',  # or "bounding_boxes"
+    "show_grid": True,
+    "show_collisions": True
 }
 
 # create and run simulation
